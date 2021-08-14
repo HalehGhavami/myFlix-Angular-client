e51e363374088f878976af975d532468888eccdb
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-08-14 14:47:52 +0200

    returned  Movies from the API and rendering all movies in movie card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { GetAllMoviesService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss']
+  styleUrls: ['./movie-card.component.scss'],
 })
 export class MovieCardComponent implements OnInit {
+  //this array is where the movies returned from the API call will be kept there
+  movies: any[] = [];
+  constructor(public fetchApiData: GetAllMoviesService) {}
 
-  constructor() { }
-
+  // After implementing the function getMovies(), it's then called in the ngOnInit() lifecycle hook
   ngOnInit(): void {
+    this.getMovies();
   }
 
+  //fetch the movies from the GetAllMoviesService with the help of getAllMovies()
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
 }
