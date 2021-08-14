dd776cb0e43fbcf2b02fd722506413e5b5f9c911
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-08-14 16:06:55 +0200

    refactored app.component and added <router-outlet>
    <router-outlet> is an element that lets Angular know which component to show for the selected route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-import { MatDialog } from '@angular/material/dialog';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,28 +6,4 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) {}
-  // This is the function that will open the dialog when the signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px',
-    });
-  }
-
-  // Directs to login modal
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      // Assigning the dialog a width
-      width: '280px',
-    });
-  }
-
-  //render all movies after user login
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px',
-    });
-  }
 }
