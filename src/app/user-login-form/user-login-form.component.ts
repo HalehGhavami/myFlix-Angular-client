daa384558ed41c3ae91e91768157669afcf90444
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-08-15 13:43:44 +0200

    changed result to Json.stringified userdata

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+// Angular Material
 // You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
-import { UserLoginService } from '../fetch-api-data.service';
-
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+
+// Cutom Services
+// This import brings in the API calls we created in 6.2
+import { UserLoginService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-login-form',
@@ -27,22 +29,22 @@ export class UserLoginFormComponent implements OnInit {
   ngOnInit(): void {}
   // This is the function responsible for sending the form inputs to the backend
   loginUser(): void {
+    // show loading
+    console.log('Loading');
     this.fetchApiData.userLogin(this.userData).subscribe(
       (result) => {
+        // hide loading
+        console.log('Done');
         // Logic for a successful login user goes here! (To be implemented)
         this.dialogRef.close(); // This will close the modal on success!
-        localStorage.setItem('user', result.user.Username);
+        localStorage.setItem('user', JSON.stringify(result.user));
         localStorage.setItem('token', result.token);
-        this.snackBar.open('User logged in successfully!', 'OK', {
-          duration: 2000,
-        });
+        console.log(result);
         this.router.navigate(['movies']);
       },
-      (result) => {
-        console.log(result);
-        this.snackBar.open(result, 'OK', {
-          duration: 2000,
-        });
+      (err) => {
+        console.log(err);
+        this.snackBar.open(`Login error occured...`, 'OK');
       }
     );
   }
