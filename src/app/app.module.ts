68fd5165c14abcb44591aa0771186f811fd8d444
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-08-18 15:54:44 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 // to use icon instead of text for Add to Favorites
 import { MatIconModule } from '@angular/material/icon';
+
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 // Angular Forms
 import { FormsModule } from '@angular/forms';
@@ -30,12 +34,18 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MovieDirectorComponent } from './movie-director/movie-director.component';
+import { MovieGenreComponent } from './movie-genre/movie-genre.component';
+import { MovieSynopsisComponent } from './movie-synopsis/movie-synopsis.component';
+import { UserProfileDeleteComponent } from './user-profile-delete/user-profile-delete.component';
+import { UserProfileUpdateComponent } from './user-profile-update/user-profile-update.component';
 // Routes
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 @NgModule({
@@ -46,7 +56,14 @@ const appRoutes: Routes = [
     MovieCardComponent,
     WelcomePageComponent,
     ProfileComponent,
+    NavBarComponent,
+    MovieDirectorComponent,
+    MovieGenreComponent,
+    MovieSynopsisComponent,
+    UserProfileDeleteComponent,
+    UserProfileUpdateComponent,
   ],
+
   imports: [
     RouterModule.forRoot(appRoutes),
     BrowserModule,
@@ -61,6 +78,9 @@ const appRoutes: Routes = [
     MatSnackBarModule,
     FormsModule,
     MatIconModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
