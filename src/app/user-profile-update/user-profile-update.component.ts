d2b023b12e6ebb84a8aa955012f00b67832fc51f
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-08-18 15:50:49 +0200

    created  profile-update component + Implement route

---
+++ src/app/user-profile-update/user-profile-update.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AppAPI } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-profile-update',
+  templateUrl: './user-profile-update.component.html',
+  styleUrls: ['./user-profile-update.component.scss'],
+})
+export class UserProfileUpdateComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+  constructor(
+    public fetchUserData: AppAPI,
+    public dialogRef: MatDialogRef<UserProfileUpdateComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  editUser(): void {
+    this.fetchUserData.editUser(this.userData).subscribe(
+      (res) => {
+        this.dialogRef.close();
+        localStorage.setItem('user', res.Username);
+        localStorage.setItem('Email', res.Email);
+        localStorage.setItem('Birthday', res.Birthday);
+        this.snackBar.open('Profile updated successfully!', 'Ok', {
+          duration: 2000,
+        });
+      },
+      (res) => {
+        console.log(res);
+        this.snackBar.open(res, 'Ok', {
+          duration: 2000,
+        });
+      }
+    );
+    setTimeout(function () {
+      window.location.reload();
+    }, 1000);
+  }
+}
