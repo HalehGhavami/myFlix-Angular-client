017a39d2eb3d9f750d00f0947659de1ff32991b0
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-08-18 15:32:06 +0200

    Added links on the movie card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,12 @@
+import { AppAPI } from './../fetch-api-data.service';
 import { Component, OnInit } from '@angular/core';
-import { AppAPI } from '../fetch-api-data.service';
+
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
+import { MovieDirectorComponent } from '../movie-director/movie-director.component';
+import { MovieGenreComponent } from '../movie-genre/movie-genre.component';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,7 +16,12 @@ import { AppAPI } from '../fetch-api-data.service';
 export class MovieCardComponent implements OnInit {
   //this array is where the movies returned from the API call will be kept there
   movies: any[] = [];
-  constructor(public fetchApiData: AppAPI) {}
+
+  constructor(
+    public fetchApiData: AppAPI,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
+  ) {}
 
   // After implementing the function getMovies(), it's then called in the ngOnInit() lifecycle hook
   ngOnInit(): void {
@@ -22,6 +34,50 @@ export class MovieCardComponent implements OnInit {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  genreDetails(Name: string, Description: string): void {
+    this.dialog.open(MovieGenreComponent, {
+      data: { Name, Description },
+    });
+  }
+
+  directorDetails(
+    Name: string,
+    Bio: string,
+    Birth: string,
+    Death: string
+  ): void {
+    this.dialog.open(MovieDirectorComponent, {
+      data: { Name, Bio, Birth, Death },
+    });
+  }
+
+  synopsisDetails(
+    Title: string,
+    ImagePath: string,
+    Description: string,
+    Director: string,
+    Genre: string
+  ): void {
+    this.dialog.open(MovieSynopsisComponent, {
+      data: { Title, ImagePath, Description, Director, Genre },
+    });
+  }
+
+  addFavorite(id: string, Title: string): void {
+    this.fetchApiData.addFavorite(id).subscribe((resp: any) => {
+      console.log(resp);
+      let favmovies = resp.FavoriteMovies;
+      localStorage.setItem('FavoriteMovies', favmovies);
+      this.snackBar.open(
+        `${Title} has been added to your favorite movies list`,
+        'OK',
+        {
+          duration: 2000,
+        }
+      );
     });
   }
 }
